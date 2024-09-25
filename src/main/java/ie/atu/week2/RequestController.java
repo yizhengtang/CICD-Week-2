1592cba0d7efbf8c870d29a0fdbdcb80d84e17e3
Yi Zheng Tang <g00425075@atu.ie>
2024-09-25 14:37:23 +0100

    Created Java Project for Week 2

---
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -0,0 +1,13 @@
+package ie.atu.week2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+    @GetMapping
+    public String sayHello()
+    {
+        return "Hi There";
+    }
+}
